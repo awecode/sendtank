25461f9d3503310d662d0ed71a7102c655c8fdc0
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-13 21:37:50 +0545

    Basic list api

--- sendtank/urls.py
+++ sendtank/urls.py
@@ -3,11 +3,19 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework.routers import DefaultRouter
+
 from apps.users import views
+from apps.tank import api as tank_api
+
+router = DefaultRouter()
+
+router.register('lists', tank_api.ListViewSet)
 
 urlpatterns = [
     url(r'^stadmin/', admin.site.urls),
     url(r'^$', views.index, name='home'),
+    url(r'^api/v1/', include(router.urls, namespace='api')),
 ]
 
 if settings.DEBUG:
