8011915296cd8170752ad7c6ea73edbf87be07af
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-13 21:45:12 +0545

    Fetching of lists and basic rendering

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import './includes/bootstrap'
 import router from './routes'
+import axios from 'axios';
+
+window.axios = axios;
 
 Vue.use(VueRouter);
+
 
 new Vue({
   el: '#app',
--- src/views/ListCollection.vue
+++ src/views/ListCollection.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
-        List Collection
+        <h1>Lists</h1>
+        <ul>
+            <li v-for="item in this.data">{{ item.name }}</li>
+        </ul>
     </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loading: false,
+        post: null,
+        error: null,
+        data: []
+      }
+    },
+    created() {
+      axios.get('/api/v1/lists/').then(({data}) => this.data = data);
+    }
+  }
+</script>
