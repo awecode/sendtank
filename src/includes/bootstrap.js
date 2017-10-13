51c1e6cfc68091e7f55eb34c0cf8a6ad9d2a2b23
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-13 20:22:27 +0545

    Create bootstrap file for Twitter Bootstrap

---
+++ src/includes/bootstrap.js
@@ -0,0 +1,5 @@
+global.jQuery = require('jquery');
+global.$ = global.jQuery;
+import Popper from '../../node_modules/popper.js/dist/umd/popper.min.js'
+global.Popper = Popper;
+require("bootstrap");
