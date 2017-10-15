158ff4a3fa3019418a0406cbae9042f0b9f1caff
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-15 13:57:56 +0545

    Create base channel class and HousedSMS channel

--- apps/send/models.py
+++ apps/send/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+# Base Channel class
+class Channel(object):
+    name = None
+
+    def __init(self):
+        if not self.name:
+            raise NotImplemented('Channel should have a name')
+
+
+class HousedSMS(Channel):
+    name = 'HousedSMS'
+
+
+channels = (
+    ('HousedSMS', HousedSMS),
+)
