c7ef7dbf0e899d153f096d741cdc65ca5d8fe8c2
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-15 20:44:43 +0545

    Cleanup

--- src/views/ListForm.vue
+++ src/views/ListForm.vue
@@ -4,7 +4,6 @@
         <vue-form :fields="fields" action="lists/">
             <template scope="form">
                 <input type="text" v-model="fields.name" id="name" class="form-control" name="name" required/>
-                <!--<field-error :error="form.errors.get('name')"></field-error>-->
                 <span class="text-danger">{{form.errors.get('name')}}</span>
             </template>
         </vue-form>
@@ -13,10 +12,9 @@
 
 <script>
   import VueForm from '../components/form/VueForm.vue';
-  import FieldError from '../components/form/FieldError.vue';
 
   export default {
-    components: {VueForm, FieldError},
+    components: {VueForm},
     data() {
       return {
         fields: {
