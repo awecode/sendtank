2c3a7cbc8f217930c3c284dd5d5fd9fe0a89a8dd
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-16 16:01:42 +0545

    Minor refactor

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 
 import App from './App.vue'
 import './includes/bootstrap'
 import './styles/main.scss'
 import router from './routes'
 import store from './store'
-
-Vue.use(VueRouter);
-
 
 new Vue({
   el: '#app',
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import VueRouter from 'vue-router'
+import Vue from 'vue'
+
 import Home from './views/Home.vue'
 import ListCollection from './views/ListCollection.vue'
 import ListForm from './views/ListForm.vue'
 import CampaignCollection from './views/CampaignCollection.vue'
 import NotFound from './views/NotFound.vue'
+
+Vue.use(VueRouter);
 
 let routes = [
   {path: '/', component: Home},
@@ -11,7 +15,7 @@ let routes = [
   {path: '/lists/create/', component: ListForm},
   {path: '/lists/update/:pk', component: ListForm},
   {path: '/campaigns/', component: CampaignCollection},
-  { path: "*", component: NotFound }
+  {path: "*", component: NotFound}
 ];
 
 export default new VueRouter({
