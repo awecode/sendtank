008c7ef83a7c95b6775744c4907dd4ac53bc1550
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-18 20:39:22 +0545

    Endpoint for user login

--- apps/users/api.py
+++ apps/users/api.py
@@ -3,8 +3,14 @@ from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework import viewsets
 
-from apps.users.models import Role
-from apps.users.serializers import RoleSerializer
+from .models import Role, User
+from .serializers import RoleSerializer, UserSerializer
+
+
+def check_required_fields(data, required_fields):
+    for field in required_fields.keys():
+        if not data.get(field):
+            raise APIException('%s field is required.' % required_fields.get(field))
 
 
 class RoleViewSet(viewsets.GenericViewSet):
@@ -19,3 +25,17 @@ class RoleViewSet(viewsets.GenericViewSet):
             return Response(RoleSerializer(role).data)
         except Role.DoesNotExist:
             raise APIException('Invalid role')
+
+
+class UserViewSet(viewsets.GenericViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    @list_route(methods=['POST'])
+    def login(self, request):
+        data = request.data
+        required_fields = {
+            'username': 'Username',
+            'password': 'Password',
+        }
+        check_required_fields(data, required_fields)
