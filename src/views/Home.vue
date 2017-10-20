681ef0816d730b5ff22babfed59703dba0ee8bd3
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-20 17:56:26 +0545

    Use Login Required mixins for views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-    import LoginRequired from "../mixins/LoginRequired";
+    import LoginRequired from "../mixins/LoginRequired"
 
     export default{
       mixins: [LoginRequired]
