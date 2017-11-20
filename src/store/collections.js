fca0855ac692490e9d33c83dc5547646be9cba39
Dipesh Acharya <xtranophilist@gmail.com>
2017-11-20 21:21:14 +0545

    Handle pagination in update_collection

--- src/store/collections.js
+++ src/store/collections.js
@@ -1,7 +1,5 @@
 export default {
-  state: {
-    'collections': {}
-  },
+  state: {},
   getters: {
     get_object: (state, getters) => (collection_name, key, key_name = 'id') => {
       let collection = state[collection_name];
@@ -12,12 +10,32 @@ export default {
   },
   mutations: {
     create_collection(state, collection_name) {
-      if (!state[collection_name]) {
-        Vue.set(state, collection_name, []);
-      }
+      Vue.set(state, collection_name, [{objects: [], pagination: {}, pages: {}}]);
     },
     update_collection(state, [collection_name, data]) {
-      Vue.set(state.collections, collection_name, data);
+      let collection = state[collection_name];
+      // Create collection if it doesn't exist
+      if (!collection) {
+        collection = Vue.set(state, collection_name, {objects: [], pagination: {}, pages: {}});
+      }
+      // Find current page
+      let page_no = data.pagination.page.toString();
+      // Update pagination
+      collection.pagination = data.pagination;
+      // Find ids of objects in current page
+      let ids = data.results.map(obj => obj.id);
+      // Remove each id from all other pages
+      ids.forEach(id => {
+        Object.keys(collection.pages).forEach(key => {
+          let page = collection.pages[key];
+          if (page.includes(id)) {
+            page = page.splice(page.indexOf(id), 1);
+          }
+        });
+      });
+      // Update current page ids
+      collection.pages[page_no] = ids;
+    // Update/add to objects list
     },
     update_collection_item(state, [collection_name, data]) {
       let collection = state[collection_name];
@@ -28,7 +46,5 @@ export default {
       }
     },
   },
-  actions: {
-    
-  },
+  actions: {},
 };
